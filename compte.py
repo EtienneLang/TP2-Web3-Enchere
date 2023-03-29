0e664009e971d1661b3f6be023262558145a203d
langloise <etiennelanglois007@gmail.com>
2023-03-29 15:09:08 -0400

    Ajout de la page jinja pour authentification

--- compte.py
+++ compte.py
@@ -6,20 +6,10 @@ import bd
 bp_compte = Blueprint('compte', __name__)
 bp_compte.secret_key = "464b2822f3de9cee02fa8a451e18c46ff3db4a0893253c0a54a527c8aa24be93"
 
-#@bp_compte.route("/authentifier", methods=["POST","GET"])
-#def authentifier():
- #   with bd.creer_connexion() as conn:
-  #      utilisateurs = bd.get_utilisateurs(conn)
-#
- #   if request.method == "POST":
-  #      utilisateur_id = request.form.get("id_utilisateur")
-   #     with bd.creer_connexion() as conn:
-    #        utilisateur = bd.get_utilisateur(conn, utilisateur_id)
-     #   session.permanent = True  # Sinon, la session sera supprimée à la fermeture du navigateur (cookie de session)
-      #  session['utilisateur'] = utilisateur
+@bp_compte.route("/authentifier", methods=["POST","GET"])
+def authentifier():
 
-
-    #return render_template("authentifier.jinja", utilisateurs = utilisateurs)
+    return render_template("authentifier.jinja")
 
 
 @bp_compte.route("/deconnecter")
