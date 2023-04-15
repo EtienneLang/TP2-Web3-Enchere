3030e116f6aa3d3b855440ce5268ed87bbaab25b
WillMatte <williamatte@gmail.com>
2023-04-15 16:59:13 -0400

    Ajustement de la route miser

--- encheres.py
+++ encheres.py
@@ -4,6 +4,7 @@ import bd
 
 bp_encheres = Blueprint('encheres', __name__)
 
+
 @bp_encheres.route("/<int:id>")
 def details(id):
     with bd.creer_connexion() as conn:
@@ -28,32 +29,42 @@ def details(id):
 
 @bp_encheres.route("/<int:id>/miser", methods=["POST"])
 def miser(id):
-    id_mise = id
+    if not session["utilisateur"]:
+        # Si l'utilisateur n'est pas connecté, on le redirige à l'autentification
+        return redirect("/compte/authentifier")
+    with bd.creer_connexion() as conn:
+        mise_max = bd.get_mise_max(conn, id)
+        enchere = bd.get_enchere(conn, id)
+    if not enchere or enchere["est_supprimee"]:
+        abort(404)
+    elif not enchere["est_active"] or enchere["fk_vendeur"] == session["utilisateur"]["id_utilisateur"]:
+        abort(400)
     erreur = False
     classe_mise = ""
-    if request.method == "POST":
-        mise_montant = request.form.get("txt_mise", type=int)
-        with bd.creer_connexion() as conn:
-            mise_max = bd.get_mise_max(conn, id_mise)
-            enchere = bd.get_enchere(conn, id)
-        if mise_montant is None:
+    mise_montant = request.form.get("txt_mise", type=int)
+    if not mise_montant:
+        classe_mise = "is-invalid"
+        texte_erreur_mise = f"Veuillez entrer une mise"
+        erreur = True
+    elif mise_montant <= 0:
+        classe_mise = "is-invalid"
+        texte_erreur_mise = f"La mise dois être plus haute que 0$"
+        erreur = True
+    elif mise_max:
+        if mise_montant <= mise_max["montant"]:
             classe_mise = "is-invalid"
-            texte_erreur_mise = f"Aucune mise entré"
+            texte_erreur_mise = f"La mise dois être plus haute que {mise_max['montant']} $"
             erreur = True
-        elif not mise_max["max"] and mise_montant <= 0:
-            classe_mise = "is-invalid"
-            texte_erreur_mise = f"La mise dois être plus haute que 0$"
-            erreur = True
-        elif mise_max["max"] is not None:
-            if mise_montant <= mise_max["max"]:
-                classe_mise = "is-invalid"
-                texte_erreur_mise = f"La mise dois être plus haute que {mise_max['max']} $"
-                erreur = True
-        if erreur:
-            return render_template("details.jinja", classe_mise=classe_mise, enchere=enchere, mise=mise_max, texte_erreur_mise=texte_erreur_mise)
-        with bd.creer_connexion() as conn:
-            bd.ajouter_mise(conn,session["utilisateur"]["id_utilisateur"], id_mise, mise_montant)
-    return redirect(f"/encheres/{id_mise}", code=303)
+    if erreur:
+        enchere["mise_max"] = mise_max["montant"]
+        return render_template("details.jinja",
+                               classe_mise=classe_mise,
+                               enchere=enchere,
+                               mise=mise_max,
+                               texte_erreur_mise=texte_erreur_mise)
+    with bd.creer_connexion() as conn:
+        bd.ajouter_mise(conn, session["utilisateur"]["id_utilisateur"], id, mise_montant)
+    return redirect(f"/encheres/{id}", code=303)
 
 
 @bp_encheres.route("/<int:id>/supprimer", methods=["POST"])
