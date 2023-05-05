f6ccf20fa78bd870784e4556fb3b0651473435e7
WillMatte <williamatte@gmail.com>
2023-05-04 21:59:51 -0400

    Ajout du défilement infinie, ça bug en criss

--- api.py
+++ api.py
@@ -2,16 +2,17 @@
 Blueprint pour le Ajax
 """
 
-from flask import Blueprint, render_template, request, redirect, session, current_app as app
+from flask import Blueprint, render_template, request, redirect, session, current_app as app, jsonify
 import bd
 
 bp_api = Blueprint('api', __name__)
+
 
 @bp_api.route("/recherche")
 def chercher_encheres():
     mot_cle = request.args.get("mot_cle")
     indice = request.args.get("indice", type=int, default=0)
     if not mot_cle:
-
-
-
+        with bd.creer_connexion() as conn:
+            encheres = bd.get_encheres(conn, indice)
+        return jsonify(encheres)
--- static/js/accueil.js
+++ static/js/accueil.js
@@ -8,34 +8,90 @@
 /**
  * Constantes globales
  * */
-const sectEnchere = document.getElementById("section-accueil")
+const sectEncheres = document.getElementById("section-accueil")
 const piedDePage = document.getElementById("sect-footer")
-
+const listeEncheres = document.getElementById("section-encheres")
 
 /**
  * Variables globales
  * */
+let controleur = null;
 let indice = 0
+let motCle
 
-function afficherEncheres()
+
+function afficherEncheres(enchere)
 {
-
+    let li = document.createElement("li")
+    let p = document.createElement("p")
+    p.append(enchere.titre)
+    li.append(p)
+    listeEncheres.append(li)
 }
 
-function defilement()
+
+async function ChercherEncheres()
 {
-    while ((innerHeight + scrollY) >= 0.9 * document.body.offsetHeight)
-    {
-        afficherEncheres()
+    let motCle = ""
+    if (controleur != null) {
+        // Annuler la requête précédente, car on lancera une nouvelle requête
+        // à chaque input et on ne veut plus le résultat de la requête précédente.
+        controleur.abort();
+    }
+
+        const parametres = {
+        "mots-cles": motCle,
+        "indice": indice
+    }
+
+    controleur = new AbortController()
+        try {
+        const encheres = await envoyerRequeteAjax(
+            "/api/recherche",
+            "GET",
+            parametres,
+            controleur
+        );
+        for (const enchere of encheres) {
+            afficherEncheres(enchere)
+            indice++
+        }
+        controleur = null
+
+
+    } catch (err) {
+        if (err.name === "AbortError") {
+            console.log("Une requête Ajax a été annulée")
+        } else {
+            console.error("Erreur lors d'une requête Ajax");
+            console.error(err);
+            //erreur.textContent = "Erreur lors de la requête. Veuillez réessayer."
+            //chargement.classList.add("masquer")
+        }
     }
 }
+
+
+async function defilement()
+{
+  while ((innerHeight + scrollY) >= 0.9 * document.body.offsetHeight)
+  {
+      await ChercherEncheres()
+  }
+}
+
+
 
 /**
  * Initialisation de la page
  */
-function initialisation()
+async function initialisation()
 {
+
     window.addEventListener('scroll', defilement)
     piedDePage.className = "d-none"
-    defilement()
+    await defilement()
+
 }
+
+window.addEventListener('load', initialisation)
