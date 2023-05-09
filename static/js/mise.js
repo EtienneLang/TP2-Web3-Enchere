79fe7713d6d6c42c3dbbce6ce4450466a179c4f8
WillMatte <williamatte@gmail.com>
2023-05-09 14:46:41 -0400

    Ajout de l'ajustement de la mise max à chaque seconde

--- static/js/mise.js
+++ static/js/mise.js
@@ -11,13 +11,17 @@ const formMise = document.getElementById("form-mise")
 const champMise = document.getElementById("txt_mise")
 const divFeedback = document.getElementById("feedback-mise")
 const spanMiseMax = document.getElementById("span-mise-max")
-
+const spanMeilleurMiseur = document.getElementById("span-meilleur-miseur")
 /**
  * Variables gloabales
  */
 let controleurMise = null;
+let miseMaxActuel
 
-
+/**
+ * Permets d'obtenir la meilleure mise sur une enchère
+ * @returns {Promise<Record<string, *>>} La meilleure mise
+ */
 async function getMiseMax() {
     try {
         return await envoyerRequeteAjax("/api/" + formMise.dataset.idEnchere + "/misemax")
@@ -31,6 +35,41 @@ async function getMiseMax() {
     }
 }
 
+/**
+ * Permets d'ajuster le span qui indique à l'utilisateur s'il a la meilleure mise
+ */
+function ajusterTexteMeilleureMiseur() {
+    if (miseMaxActuel.fk_miseur === Number(spanMeilleurMiseur.dataset.idUtilisateur)) {
+        spanMeilleurMiseur.innerText = "Vous avez la meilleure mise"
+    } else {
+        spanMeilleurMiseur.innerText = "Vous n'avez pas la meilleure mise"
+    }
+}
+
+/**
+ * Permets d'ajuster l'affichage de la meilleure mise
+ * @returns {Promise<void>}
+ */
+async function ajusterMiseMax() {
+    let miseMax = await getMiseMax()
+    if (miseMax === null) {
+        return
+    }
+    if (miseMax.montant === miseMaxActuel.montant) {
+        if (spanMeilleurMiseur.innerText === "")
+            ajusterTexteMeilleureMiseur()
+        return
+    }
+    miseMaxActuel = miseMax
+    champMise.min = miseMaxActuel.montant
+    spanMiseMax.innerText = miseMaxActuel.montant + "$"
+    ajusterTexteMeilleureMiseur()
+}
+
+/**
+ * Permets de valider si une mise est valide
+ * @returns {Promise<boolean>} True si la mise est valide, False sinon
+ */
 async function validerMise() {
     champMise.className = ""
     divFeedback.innerHTML = ""
@@ -41,7 +80,7 @@ async function validerMise() {
         return false
     }
     miseUtilisateur = Number(miseUtilisateur)
-    //On ne devrait jamais rentrer ici car le input est en type number, mais là pour être certain
+    //On ne devrait jamais rentrer ici, car le input est en type number, mais là pour être certain
     if (isNaN(miseUtilisateur)) {
         champMise.classList.add("is-invalid")
         divFeedback.innerHTML = "<p>La mise doit être une valeur numérique entière</p>"
@@ -63,11 +102,18 @@ async function validerMise() {
     return true
 }
 
+/**
+ * Permets de miser sur une enchère
+ * @param e sender de l'événement
+ * @returns {Promise<void>}
+ */
 async function miserSurEnchere(e) {
     e.preventDefault()
     if (await validerMise()) {
         if (controleurMise != null) {
             //on attend que la dernière requète soit completé pour en envoyer une autre
+            champMise.classList.add("is-invalid")
+            divFeedback.innerHTML = "<p>Veuillez attendre que votre mise dernière mise soit completée</p>"
             return;
         }
         const parametres = {
@@ -75,15 +121,15 @@ async function miserSurEnchere(e) {
         }
         controleurMise = new AbortController()
         try {
-             const miseMax = await envoyerRequeteAjax(
+            const miseMax = await envoyerRequeteAjax(
                 "/api/miser/" + formMise.dataset.idEnchere,
                 "POST",
                 parametres,
                 controleurMise
-             );
-             champMise.value = ""
-             spanMiseMax.innerText = miseMax.montant + "$"
-             controleurMise = null;
+            );
+            champMise.value = ""
+            await ajusterMiseMax()
+            controleurMise = null;
         } catch (err) {
             console.error("Erreur lors d'une mise");
             console.error(err);
@@ -93,11 +139,17 @@ async function miserSurEnchere(e) {
     }
 }
 
-function initialisation() {
+/**
+ * Initialisation de la page
+ * @returns {Promise<void>}
+ */
+async function initialisation() {
+    //Quand l'utilisateur ne peut pas miser le formulaire n'existe pas
     if (formMise !== null) {
         formMise.addEventListener("submit", miserSurEnchere)
     }
-
+    miseMaxActuel = await getMiseMax()
+    setInterval(ajusterMiseMax, 1000);
 }
 
 window.addEventListener("load", initialisation)
