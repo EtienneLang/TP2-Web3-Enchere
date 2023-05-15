97b385d81da39eae97e5d492b6325fc115078303
WillMatte <williamatte@gmail.com>
2023-05-15 11:27:40 -0400

    Ajout des loggers dans l'api et touches finales du TP

--- api.py
+++ api.py
@@ -20,9 +20,11 @@ def chercher_encheres():
         est_admin = False
     if not mot_cle or mot_cle == "":
         with bd.creer_connexion() as conn:
+            app.logger.info("L'utilisateur va chercher des enchères sur l'accueil sans mot clé")
             encheres = bd.get_encheres(conn, indice, est_admin)
             return jsonify(encheres)
     with bd.creer_connexion() as conn:
+        app.logger.info("L'utilisateur va chercher des enchères sur l'accueil avec mot clé")
         encheres = bd.chercher_encheres(conn, mot_cle, indice, est_admin)
         return jsonify(encheres)
 
@@ -30,15 +32,18 @@ def chercher_encheres():
 @bp_api.route("/miser/<int:id_enchere>", methods=["POST"])
 def miser(id_enchere):
     if not session or not session["utilisateur"]:
+        app.logger.warning("Un utilisateur non indentifié éssaie de miser sur une enchère")
         abort(401)
     app.logger.info("Un utilisateur éssaie de miser")
     with bd.creer_connexion() as conn:
         enchere = bd.get_enchere(conn, id_enchere)
         mise_max = bd.get_mise_max(conn, id_enchere)
         if not enchere or enchere["est_supprimee"]:
+            app.logger.warning("Un utilisateur éssaie de misé sur une enchère non existante ou supprimé")
             abort(404)
         if not enchere["est_active"] or enchere["fk_vendeur"] == session["utilisateur"]["id_utilisateur"]:
-            abort(400)
+            app.logger.warning("Un utilisateur éssaie de misé sur une enchère inactive ou une de ses enchère")
+            abort(403)
     mise_montant = request.form.get("txt_mise", type=int)
     if not mise_montant or mise_montant <= 0:
         abort(400)
@@ -60,6 +65,8 @@ def get_mise_max(id_enchere):
     with bd.creer_connexion() as conn:
         enchere = bd.get_enchere(conn, id_enchere)
         if not enchere:
+            app.logger.warning("Un utilisateur éssaie d'avoir la mise max d'une enchère inexistante")
             abort(404)
         mise_max = bd.get_mise_max(conn, id_enchere)
+        app.logger.info("L'API renvois la mise max d'une enchère")
         return jsonify(mise_max)
